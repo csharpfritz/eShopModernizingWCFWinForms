aaaf7260384fad4439d61bb29d62496c9e2d31ed
Aditi Dugar <adugar@microsoft.com>
2017-09-19 11:49:57 -0700

    Add Discount Service functionality

---
+++ eShopWinForms/DiscountService/DiscountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountService
+{
+    public class DiscountService
+    {
+        public static Discount GetDiscount()
+        {
+            var discount = new Discount(0.1, new DateTime().AddDays(10));
+            return discount;
+        }
+    }
+}
---
+++ eShopWinForms/eShopWinForms/Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountService
+{
+    public class Discount
+    {
+        public Discount(double size, DateTime ends)
+        {
+            Size = size;
+            Ends = ends;
+        }
+
+        public double Size { get; private set; }
+
+        public DateTime Ends { get; private set; }
+    }
+}
